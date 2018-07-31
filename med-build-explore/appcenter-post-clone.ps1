0d5b080cecf6b7b1d2735c75a089bcb44f9b4f4e
Mark Davis <gitkraken@nohorse.com>
2018-07-31 15:07:15 -0700

    those darn quotes

--- med-build-explore/appcenter-post-clone.ps1
+++ med-build-explore/appcenter-post-clone.ps1
@@ -1,2 +1,5 @@
-﻿$filePath = ".\med-build-explore\med-build-explore\Config.cs"
-(Get-Content $filePath).Replace('SecretParm',$env:SAMPLESECRET) | Set-Content $filePath
+﻿Write-Host $env:APPCENTER_UWP_SOLUTION
+$filePath = ".\med-build-explore\med-build-explore\Config.cs"
+Write-Host $filepath
+Write-Host $env:SAMPLESECRET
+(Get-Content $filePath).Replace('SecretParm','$env:SAMPLESECRET') | Set-Content $filePath
